c74d31493f5e8101088580f13beb240a8b613304
Emily Colon <emiquigs@gmail.com>
2022-02-11 10:59:52 -0500

    .env and user page progress below search and login

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -20,21 +20,17 @@ const Dashboard = () => {
             // justifyContent: 'Left',
             // height: '100vh'
         }}>
-
-        
-        {/* <Stack direction="horizontal" gap={3}>  */}
         <Sidebar/>
-        <Outlet />
-
         <Container>
             <Row className="justify-content-md-center">
 
                 <Col md={{ span: 6, offset: 0 }} sm ={6} xs={6}><GoogleLoginComponent/></Col>
                 <Col md={{ span: 4, offset: 5 }} sm ={6} xs={6}><Books/></Col>
             </Row>
+            <Row>
+                <Col><Outlet /></Col>
+            </Row>
         </Container>
-        {/* </Stack> */}
-
     </div>
     );
 };
