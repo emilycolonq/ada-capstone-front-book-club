9e692edbe388c51f2271bc8669b92420082f663e
Emily Colon <emiquigs@gmail.com>
2022-02-11 00:07:35 -0500

    bootstrap progress bar

--- src/components/group.js
+++ src/components/group.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 // import PropTypes from 'prop-types';
-import PagesProgressBar from './pagesprogress';
+import PercentBar from './pagesprogress';
 import { Row, Col } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 
-const Group = (props) => {
+
+const MAX_CORRECT = 150;
+
+export default function Group() {
+    const [maxScore] = useState(MAX_CORRECT);
+    const [score, setScore] = useState(0);
+
+    useEffect(() => {
+        setScore(prevScore => Math.max(0, Math.min(prevScore, maxScore)));
+    }, [maxScore]);
+
+    const fetchData = correct => {
+        correct && setScore(score => score + 1);
+    };
+
+    const randomTrueFalse = () => Math.random() >= 0.333;
+
+    const scoreLabel = `${score}/${maxScore}`;
+    const percentCorrect = (score * 100) / maxScore;
+    const percentCorrectLabel = `${Number(percentCorrect).toFixed(2)}%`;
+
+
+    return (
+        <div className="App">
+            <Row className="justify-content-md-center">
+                <Col md={{ span: 6, offset: 0 }} sm ={6} xs={6}>
+                    <h1>User Page Progress</h1>
+                    <div>Page {score} out of {maxScore} total pages</div>
+                    <ProgressBar
+                        animated
+                        min={0}
+                        max={maxScore}
+                        now={score}
+                        label={scoreLabel}
+                    />
+                    <div>Percentage finished</div>
+                    <PercentBar label={percentCorrectLabel} percent={percentCorrect} />
+
+                    <button type="button" onClick={() => fetchData(randomTrueFalse())}>
+                        Correct or Not?
+                    </button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+// const Group = (props) => {
     
     // const selectGroup = () => {
     //     const newSelected = {
@@ -13,19 +63,18 @@ const Group = (props) => {
     //     }
     //     props.current(newSelected);
     // } 
-    return (
-        <div>
-            <Row className="justify-content-md-center">
-                <Col md={{ span: 6, offset: 0 }} sm ={6} xs={6}>
-                    <h3 className="page-progress">Progress Bar</h3>
-                        <PagesProgressBar bgcolor="yellow" progress='30'  height={30} />
-                        <PagesProgressBar bgcolor="orange" progress='60'  height={30} />
-                </Col>
-            </Row>
-        </div>
-        // <h1 className="group-item" onClick={selectGroup}> {props.group.name} </h1>
-    )
+//     return (
+//         <div>
+//             <Row className="justify-content-md-center">
+//                 <Col md={{ span: 6, offset: 0 }} sm ={6} xs={6}>
+//                     <h3 className="page-progress">Progress Bar</h3>
+//                         <PagesProgressBar bgcolor="yellow" progress='30'  height={30} />
+//                         <PagesProgressBar bgcolor="orange" progress='60'  height={30} />
+//                 </Col>
+//             </Row>
+//         </div>
+//         <h1 className="group-item" onClick={selectGroup}> {props.group.name} </h1>
+//     )
+// }
 
-}
-
-export default Group;
+// export default Group;
