4bae6cf1975d38add74900e9ad207191e6b55de4
Emily Colon <emiquigs@gmail.com>
2022-02-15 13:29:52 -0500

    trying things on post call

--- src/components/NewGroupForm.js
+++ src/components/NewGroupForm.js
@@ -8,20 +8,23 @@ import axios from "axios";
 const NewGroupForm = (props) => {
 
     const [formValue, setformValue] = React.useState({
-        name: ''
+        name: '',
+        title: props.title ,
+        author: props.author,
+        pages: props.page
     });
 
-    const handleSubmit = async() => {
+    const handleSubmit = () => {
         // store the states in the form data
         const groupFormData = new FormData();
         groupFormData.append("group_name", formValue.name)
-        groupFormData.append("book_title", props.title)
-        groupFormData.append("book_author", props.author)
-        groupFormData.append("book_pages", props.page)
+        groupFormData.append("book_title", formValue.title)
+        groupFormData.append("book_author", formValue.author)
+        groupFormData.append("book_pages", formValue.pages)
     
         try {
           // make axios post request
-            const response = await axios({
+            const response = axios({
                 method: "post",
                 url: "https://ada-capstone-book-club.herokuapp.com/adabookclub/groups/",
                 data: groupFormData,
@@ -42,13 +45,32 @@ const NewGroupForm = (props) => {
     return (
         <form onSubmit={handleSubmit}>
         <h2>Create Group</h2>
-        <p>{props.title} by {props.author} with {props.page} pages</p>
+        {/* <p>{props.title} by {props.author} with {props.page} pages</p> */}
         <input
             type="text"
             name="name"
-            value={formValue.name}
-            onChange={handleChange}
+            value={formValue.title}
+            // onChange={handleChange}
         />
+        <input
+            type="text"
+            name="name"
+            value={formValue.author}
+            // onChange={handleChange}
+        />
+        <input
+            type="text"
+            name="name"
+            value={formValue.pages}
+            // onChange={handleChange}
+        />
+        <h3>Enter Group Name Here:</h3>
+            <input
+                type="text"
+                name="name"
+                value={formValue.name}
+                onChange={handleChange}
+            />
         <button type="submit">
             Submit
         </button>
